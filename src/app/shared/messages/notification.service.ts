d60e10ad0cd27c400b24c47acca241f372e97295
FlexaBR <r.gutheil@terra.com.br>
2018-10-19 16:43:58 -0300

    ate aula 92

---
+++ src/app/shared/messages/notification.service.ts
@@ -0,0 +1,12 @@
+import { EventEmitter } from '@angular/core'
+
+export class NotificationService {
+
+  constructor() { }
+
+  notifier = new EventEmitter<any>()
+
+  notify(message: string) {
+    this.notifier.emit(message)
+  }
+}
